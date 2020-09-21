c27c31bf81fff66f09d9ba4b757c9b83e55b033b
Vamanan <vamanan.arulchelvan@stud.hslu.ch>
2020-09-21 14:36:09 +0200

    Test PyCharm

--- examples/arena/arena_play.py
+++ examples/arena/arena_play.py
@@ -9,15 +9,17 @@ import numpy as np
 from jass.agents.agent import Agent
 from jass.arena.arena import Arena
 from jass.agents.agent_random_schieber import AgentRandomSchieber
-from jass.game.const import color_masks, card_strings
+from jass.game.const import color_masks, card_strings, card_values
 from jass.game.game_observation import GameObservation
 from jass.game.rule_schieber import RuleSchieber
 
-#Hallo 123
+
+# Hallo 123
 class MyAgent(Agent):
     """
     Sampl implemntation of a player to play Jass.
     """
+
     def __init__(self):
         # log actions
         self._logger = logging.getLogger(__name__)
@@ -45,14 +47,46 @@ class MyAgent(Agent):
         return card
 
 
+class MyAgent2(Agent):
+    """
+    SW1 - Excercise
+    """
+
+    def __init__(self):
+        # log actions
+        self._logger = logging.getLogger(__name__)
+        # Use rule object to determine valid actions
+        self._rule = RuleSchieber()
+        # init random number generator
+        self._rng = np.random.default_rng()
+
+    def action_trump(self, obs: GameObservation) -> int:
+        trump = 0
+        max_number_in_color = 0
+        for c in range(4):
+            number_in_color = (obs.hand * card_values[c]).sum()
+            if number_in_color > max_number_in_color:
+                max_number_in_color = number_in_color
+                trump = c
+        return trump
+
+    def action_play_card(self, obs: GameObservation) -> int:
+        # cards are one hot encoded
+        valid_cards = self._rule.get_valid_cards_from_obs(obs)
+        # convert to list and draw a value
+        card = self._rng.choice(np.flatnonzero(valid_cards))
+        self._logger.debug('Played card: {}'.format(card_strings[card]))
+        return card
+
+
 def main():
     # Set the global logging levl (Set to debug or info to see more messages)
     logging.basicConfig(level=logging.WARNING)
 
     # setup the arena
-    arena = Arena(nr_games_to_play=10000, save_filename='arena_games')
-    player = MyAgent()#AgentRandomSchieber()
-    my_player = MyAgent()
+    arena = Arena(nr_games_to_play=1000, save_filename='arena_games')
+    player = MyAgent2()  # Team 1# AgentRandomSchieber()
+    my_player = MyAgent()           # Team 0
 
     arena.set_players(my_player, player, my_player, player)
     print('Playing {} games'.format(arena.nr_games_to_play))
