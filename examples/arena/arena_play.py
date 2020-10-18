9a9c1358da0adf3445f018615a23df0c9742fbe7
Vamanan <vamanan.arulchelvan@stud.hslu.ch>
2020-10-18 19:57:17 +0200

    Test PyCharm

--- examples/arena/arena_play.py
+++ examples/arena/arena_play.py
@@ -13,12 +13,35 @@ from jass.game.const import color_masks, card_strings, card_values
 from jass.game.game_observation import GameObservation
 from jass.game.rule_schieber import RuleSchieber
 
+#from mcts.my_mcts_player import MyMCTSPlayer
 
-# Hallo 123
-class MyAgent(Agent):
+#from mcts.my_mcts_player_random_trump import MyIMCTSPlayerRandomTrump
+#from my_jass.player.my_player_deep_trump import MyPlayerDeepTrump
+#from my_jass.ml_player.ml_player import MyMLPlayer
+#from my_jass.imcts.my_imcts_deep_player import MyIMCTSDeepPlayer
+#from my_jass.player.my_player import MyPlayer
+import joblib
+import pandas as pd
+#from sklearn.model_selection import GridSearchCV
+
+
+
+def trump_c(i):
+    switcher = {
+        "['DIAMONDS']": 0,
+        "['HEARTS']": 1,
+        "['SPADES']": 2,
+        "['CLUBS']": 3,
+        "['OBE_ABE']": 4,
+        "['UNE_UFE']": 5,
+    }
+    return switcher.get(i)
+
+class MyAgent2(Agent):
     """
     Sampl implemntation of a player to play Jass.
     """
+
 
     def __init__(self):
         # log actions
@@ -27,16 +50,21 @@ class MyAgent(Agent):
         self._rule = RuleSchieber()
         # init random number generator
         self._rng = np.random.default_rng()
+        self.model_clone = joblib.load('my_model.pkl')
 
     def action_trump(self, obs: GameObservation) -> int:
         trump = 0
         max_number_in_color = 0
-        for c in range(4):
-            number_in_color = (obs.hand * color_masks[c]).sum()
-            if number_in_color > max_number_in_color:
-                max_number_in_color = number_in_color
-                trump = c
-        return trump
+        # for c in range(4):
+        #     number_in_color = (obs.hand * color_masks[c]).sum()
+        #     if number_in_color > max_number_in_color:
+        #         max_number_in_color = number_in_color
+        #         trump = c
+        cards_tr = self._rule.get_valid_cards_from_obs(obs)
+        cards_tr =np.append(cards_tr,1).astype(bool)
+        X_test_2 = np.transpose((pd.DataFrame(cards_tr)).values)
+        X_test_3 = (pd.DataFrame(np.array([[True] * 37]))).values
+        return trump_c(np.array2string(self.model_clone.predict(X_test_2)))
 
     def action_play_card(self, obs: GameObservation) -> int:
         # cards are one hot encoded
@@ -47,7 +75,7 @@ class MyAgent(Agent):
         return card
 
 
-class MyAgent2(Agent):
+class MyAgent(Agent):
     """
     SW1 - Excercise
     """
@@ -85,10 +113,11 @@ def main():
 
     # setup the arena
     arena = Arena(nr_games_to_play=1000, save_filename='arena_games')
-    player = MyAgent2()  # Team 1# AgentRandomSchieber()
-    my_player = MyAgent()           # Team 0
+    random_player = MyAgent()  # Team 1 # AgentRandomSchieber()
+    my_player_1 = MyAgent2()    # Team 0 #billiher bot vom 1.Semester
+    #mcts_player = MyMCTSPlayer()
 
-    arena.set_players(my_player, player, my_player, player)
+    arena.set_players(my_player_1, random_player, my_player_1, random_player)
     print('Playing {} games'.format(arena.nr_games_to_play))
     arena.play_all_games()
     print('Average Points Team 0: {:.2f})'.format(arena.points_team_0.mean()))
